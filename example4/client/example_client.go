42ee1b89015765ea0607c09b0443cb0c48e6d622
Ivar Johansson <ivar@webbmaffian.se>
2023-09-21 15:35:42 +0200

    Stress tests

--- example4/client/example_client.go
+++ example4/client/example_client.go
@@ -6,8 +6,10 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"sync"
 	"time"
 
+	"github.com/rs/xid"
 	"github.com/webbmaffian/go-logger"
 	"github.com/webbmaffian/go-logger/example3"
 	"github.com/webbmaffian/go-logger/peer"
@@ -17,13 +19,19 @@ func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
 	defer stop()
 
-	var certs example3.Certs
+	// var certs example3.Certs
 
-	if err := certs.LoadOrCreate("../certs", "localhost"); err != nil {
-		return
+	// if err := certs.LoadOrCreate("../certs", "localhost"); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// if err := startClient(ctx, &certs); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	if err := startLiveClient(ctx); err != nil {
+		log.Fatal(err)
 	}
-
-	startClient(ctx, &certs)
 }
 
 func startClient(ctx context.Context, certs *example3.Certs) (err error) {
@@ -81,6 +89,98 @@ func startClient(ctx context.Context, certs *example3.Certs) (err error) {
 
 	os.Stdout.WriteString("\n")
 
+	log.Println("done")
+
+	log.Println("waiting 3 seconds")
+	time.Sleep(time.Second * 3)
+
+	return
+}
+
+func startLiveClient(ctx context.Context) (err error) {
+	var (
+		pool   *logger.Pool
+		client logger.Client
+		certs  example3.Certs
+	)
+
+	if err = certs.RootCa.FromFile("../cert-live/root-ca.pem"); err != nil {
+		return
+	}
+
+	if err = certs.ClientKey.FromFile("../cert-live/private.key"); err != nil {
+		return
+	}
+
+	if err = certs.ClientCert.FromFile("../cert-live/certificate.pem"); err != nil {
+		return
+	}
+
+	log.Println("starting client")
+
+	if client, err = peer.NewTlsClient(ctx, peer.TlsClientOptions{
+		Address:     "wm.log.center:4610",
+		PrivateKey:  certs.ClientKey,
+		Certificate: certs.ClientCert,
+		RootCa:      certs.RootCa,
+		BufferSize:  500,
+		ErrorHandler: func(err error) {
+			log.Println("client:", err)
+		},
+		// Debug: func(msg string) {
+		// 	log.Println("client:", msg)
+		// },
+	}); err != nil {
+		return
+	}
+
+	if pool, err = logger.NewPool(client, logger.PoolOptions{
+		BucketId: 1695284931,
+	}); err != nil {
+		return
+	}
+
+	// log.Println("all set up")
+	l := pool.Logger().Cat(1).Tag("127.0.0.1", "foo@bar.baz", xid.New())
+
+	// log.Println("waiting 3 seconds")
+	// time.Sleep(time.Second * 3)
+
+	// log.Println("waiting 3 seconds")
+	// time.Sleep(time.Second * 3)
+
+	var wg sync.WaitGroup
+
+	for worker := 0; worker < 1; worker++ {
+		wg.Add(1)
+		go func(worker int) {
+			for i := 0; i < 100; i++ {
+				l.Info("Msg %d from worker %d").Tag(i+1, worker+1, "a", "b", "c", "d", "e").Meta("Specific error", "räksmörgås").Send()
+				time.Sleep(time.Millisecond)
+			}
+
+			wg.Done()
+		}(worker)
+	}
+
+	// for i := 0; i < 10; i++ {
+	// 	if err = ctx.Err(); err != nil {
+	// 		return
+	// 	}
+
+	// 	// entry := l.Debug("msg %s", strconv.Itoa(i))
+	// 	// entry :=
+
+	// 	// os.Stdout.WriteString(fmt.Sprintf("Sent %4d\r", i+1))
+	// 	// time.Sleep(time.Second * 6)
+
+	// 	// log.Println("\n" + example3.FormatEntry(entry, "<"))
+	// 	// time.Sleep(time.Second)
+	// }
+
+	// os.Stdout.WriteString("\n")
+
+	wg.Wait()
 	log.Println("done")
 
 	log.Println("waiting 3 seconds")
